e4b715f8aed12793a3186ab8aefe3dc19d63cfc7
JozefTkocz <49951089+JozefTkocz@users.noreply.github.com>
2022-01-07 16:10:36 +0000

    update package requirements

--- steup.py
+++ steup.py
@@ -1,15 +1,25 @@
 import os
+
 from setuptools import setup
+
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
+def read_requirements(requirements_file):
+    with open(requirements_file) as f:
+        required_packages = f.read().splitlines()
+    return required_packages
+
+
 setup(
-    name = "an_example_pypi_project",
-    version = "0.0.1",
-    author = "Jozef Tkocz",
-    author_email = "jozeftkocz@gmail.com",
-    description = ("Python wrapper for the weather API provided by https://www.weatherapi.com/"),
-    packages=['an_example_pypi_project', 'tests'],
+    name="an_example_pypi_project",
+    python_requires=">3.8,<4.0",
+    version="0.0.1",
+    author="Jozef Tkocz",
+    author_email="jozeftkocz@gmail.com",
+    description=("Python wrapper for the weather API provided by https://www.weatherapi.com/"),
+    install_requires=read_requirements('requirements.txt'),
     long_description=read('README')
 )
