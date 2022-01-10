101b0d670ad501d7d62e907551552989c0315161
JozefTkocz <49951089+JozefTkocz@users.noreply.github.com>
2022-01-10 14:42:53 +0000

    test return type of endpoint method

---
+++ tests/test_endpoint_methods.py
@@ -0,0 +1,29 @@
+import datetime as dt
+import json
+import os
+from unittest.mock import patch
+
+import pandas as pd
+
+from weatherapi import get_weather_history
+from weatherapi.web_api.interface import APICaller
+
+TEST_FOLDER = os.path.dirname(__file__)
+DATA_FOLDER = os.path.join(TEST_FOLDER, 'data')
+
+ANY_FLOAT = 0.01
+ANY_INT = 2
+
+example_json_data_filepath = os.path.join(DATA_FOLDER, 'example.json')
+
+with open(example_json_data_filepath, 'r') as file:
+    example_data = json.load(file)
+
+
+@patch.object(APICaller, "get_history")
+def test_get_weather_history(get_history):
+    get_history.return_value = example_data
+    result = get_weather_history(api_key='A_STRING', latitude=ANY_FLOAT, longitude=ANY_FLOAT,
+                                 start_time=dt.datetime(year=2022, month=1, day=7),
+                                 end_time=None)
+    assert type(result) == pd.DataFrame
