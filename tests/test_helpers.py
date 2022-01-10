bc449d2dfdd7ff6a3573d17f18e0f3687da7842c
JozefTkocz <49951089+JozefTkocz@users.noreply.github.com>
2022-01-10 12:47:46 +0000

    add tests for date range generation

---
+++ tests/test_helpers.py
@@ -0,0 +1,46 @@
+import datetime as dt
+
+import pandas as pd
+import pytest
+from pandas.testing import assert_index_equal
+
+from weatherapi.helpers import generate_date_range_for_period
+
+
+def test_generate_date_range_for_period_multiple_days():
+    start_date = dt.datetime(year=2022, month=1, day=1, hour=12)
+    end_date = dt.datetime(year=2022, month=1, day=2, hour=5)
+
+    expected_result = pd.DatetimeIndex([pd.Timestamp(start_date).date(), pd.Timestamp(end_date).date()], freq='D')
+    actual_result = generate_date_range_for_period(start_date, end_date)
+
+    assert_index_equal(expected_result, actual_result)
+
+
+def test_generate_date_range_for_period_only_start_date():
+    start_date = dt.datetime(year=2022, month=1, day=1, hour=12)
+    end_date = None
+
+    expected_result = pd.DatetimeIndex([pd.Timestamp(start_date).date()], freq='D')
+    actual_result = generate_date_range_for_period(start_date, end_date)
+
+    assert_index_equal(expected_result, actual_result)
+
+
+def test_generate_date_range_for_period_start_and_end_are_same_day():
+    start_date = dt.datetime(year=2022, month=1, day=1, hour=10)
+    end_date = dt.datetime(year=2022, month=1, day=1, hour=15)
+
+    # result should be a daterange index containing two dates
+    expected_result = pd.DatetimeIndex([pd.Timestamp(start_date).date()], freq='D')
+    actual_result = generate_date_range_for_period(start_date, end_date)
+
+    assert_index_equal(expected_result, actual_result)
+
+
+def test_generate_date_range_for_period_raises_exception_if_end_before_start():
+    start_date = dt.datetime(year=2022, month=1, day=2, hour=12)
+    end_date = dt.datetime(year=2022, month=1, day=1, hour=12)
+
+    with pytest.raises(Exception) as e_info:
+        generate_date_range_for_period(start_date, end_date)
--- weatherapi/helpers.py
+++ weatherapi/helpers.py
@@ -2,11 +2,17 @@ import datetime as dt
 
 import pandas as pd
 
+from typing import Union
 
-def generate_date_range_for_period(start_time: dt.datetime, end_time: dt.datetime) -> pd.DatetimeIndex:
+
+def generate_date_range_for_period(start_time: dt.datetime, end_time: Union[dt.datetime, None]) -> pd.DatetimeIndex:
     if end_time is None:
-        end_time = start_time + pd.to_timedelta('1D')
+        end_time = start_time
+
+    assert start_time <= end_time, 'start date must be before end date.'
+
     start_date = pd.Timestamp(start_time).floor('1D')
     end_date = pd.Timestamp(end_time).ceil('1D') - pd.to_timedelta('1D')
+
     date_range = pd.date_range(start=start_date, end=end_date, freq='1D')
     return date_range
