26ff9b14b4ec148d3615d07e7f2bb1adae569178
JozefTkocz <49951089+JozefTkocz@users.noreply.github.com>
2022-01-10 12:11:51 +0000

    use find_packages to import submodules

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def read(fname):
@@ -20,7 +20,7 @@ setup(
     author="Jozef Tkocz",
     author_email="jozeftkocz@gmail.com",
     description=("Python wrapper for the weather API provided by https://www.weatherapi.com/"),
-    packages=['weatherapi'],
+    packages=[find_packages()],
     install_requires=read_requirements('requirements.txt'),
     long_description=read('README.md')
 )
