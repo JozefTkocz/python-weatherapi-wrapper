caa4ddd7a9133bc5614b79cfee468b0964b0f296
JozefTkocz <49951089+JozefTkocz@users.noreply.github.com>
2022-01-10 12:20:40 +0000

    return data from api if only start date is provided

--- weatherapi/helpers.py
+++ weatherapi/helpers.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 def generate_date_range_for_period(start_time: dt.datetime, end_time: dt.datetime) -> pd.DatetimeIndex:
     if end_time is None:
-        end_time = start_time
+        end_time = start_time + pd.to_timedelta('1D')
     start_date = pd.Timestamp(start_time).floor('1D')
     end_date = pd.Timestamp(end_time).ceil('1D') - pd.to_timedelta('1D')
     date_range = pd.date_range(start=start_date, end=end_date, freq='1D')
